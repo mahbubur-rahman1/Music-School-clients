a5e43d4e2ea4708eb33d419b450027a776120738
mahbubur-rahman1 <you@example.com>
2023-06-12 22:35:16 +0600

    last final update

--- src/page/Shaerd/Navbar/Navbar.jsx
+++ src/page/Shaerd/Navbar/Navbar.jsx
@@ -24,8 +24,8 @@ const Navbar = () => {
     </>
     return (
         <nav>
-            <div className="navbar w-full   z-30 text-white bg-opacity-50 bg-black">
-                <div className="navbar-start ">
+            <div className="navbar w-full  text-white bg-opacity-50 bg-black">
+                <div className="navbar-start z-10">
                     <div className="dropdown">
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
